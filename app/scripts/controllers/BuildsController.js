5274556cc0f234e8f9f136ed6e1dbc10e92599f2
rodrigo brancher <rbrancher@gmail.com>
2013-10-22 19:10:31 -0200

    in progress

---
+++ app/scripts/controllers/BuildsController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+bb_app.controller('BuildsController', function ($scope, $stateParams) {
+
+  $scope.bike_type = $stateParams.type;
+
+});
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -40,4 +40,3 @@ angular.module('bikebuilderApp')
     $urlRouterProvider.otherwise('/');
 
   }]);
-  
